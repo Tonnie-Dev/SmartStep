bac7002161aa6bd3ad951b3ceb20fc9b1f2c693e
Tonnie <vontonnie@gmail.com>
2026-03-01 17:47:20 +0300

    Add Typography with Inter font family

--- app/src/main/java/com/tonyxlab/smartstep/presentation/theme/Type.kt
+++ app/src/main/java/com/tonyxlab/smartstep/presentation/theme/Type.kt
@@ -1,34 +1,116 @@
 package com.tonyxlab.smartstep.presentation.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tonyxlab.smartstep.R
 
-// Set of Material typography styles to start with
+private val InterFontFamily = FontFamily(
+    Font(R.font.inter_variable_font, weight = FontWeight.Normal),
+    Font(R.font.inter_variable_font, weight = FontWeight.Medium),
+    Font(R.font.inter_variable_font, weight = FontWeight.SemiBold)
+)
+
+
 val Typography = Typography(
-        bodyLarge = TextStyle(
-                fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = InterFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = InterFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = InterFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = InterFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+)
+
+object ExtraTypography {
+
+        val TitleAccent = TextStyle(
+        fontFamily = InterFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 64.sp,
+        lineHeight = 70.sp
+        )
+
+        val BodyLargeRegular = TextStyle(
+                fontFamily = InterFontFamily,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp
+                lineHeight = 24.sp
         )
-        /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
+
+       val BodyLargeMedium = TextStyle(
+               fontFamily = InterFontFamily,
+               fontWeight = FontWeight.Medium,
+               fontSize = 16.sp,
+               lineHeight = 24.sp
+       )
+
+        val BodyMediumRegular = TextStyle(
+                fontFamily = InterFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                lineHeight = 18.sp
+        )
+
+        val BodyMediumMedium = TextStyle(
+                fontFamily = InterFontFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
+                lineHeight = 18.sp
+        )
+        val BodySmallRegular = TextStyle(
+                fontFamily = InterFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                lineHeight = 16.sp
+        )
+}
+
+
+val Typography.TittleAccent
+        @Composable
+        get() = ExtraTypography. TitleAccent
+
+val Typography.BodyLargeRegular
+        @Composable
+        get() = ExtraTypography. BodyLargeRegular
+
+val Typography.BodyLargeMedium
+        @Composable
+        get() = ExtraTypography. BodyLargeMedium
+
+val Typography.BodyMediumRegular
+       @Composable
+        get() = ExtraTypography. BodyMediumRegular
+
+val Typography.BodyMediumMedium
+        @Composable
+        get() = ExtraTypography. BodyMediumMedium
+
+val Typography.BodySmallRegular
+        @Composable
+        get() = ExtraTypography. BodySmallRegular
