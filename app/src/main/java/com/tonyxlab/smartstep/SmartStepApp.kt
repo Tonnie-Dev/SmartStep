ea2bd1e20d6ec983975c7405faeda169562b62d9
Tonnie <vontonnie@gmail.com>
2026-03-11 20:46:17 +0300

    Add Application Class

---
+++ app/src/main/java/com/tonyxlab/smartstep/SmartStepApp.kt
@@ -0,0 +1,24 @@
+package com.tonyxlab.smartstep
+
+import android.app.Application
+import com.tonyxlab.smartstep.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class SmartStepApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+if (BuildConfig.DEBUG){
+    Timber.plant(Timber.DebugTree())
+}
+
+        startKoin {
+            androidLogger()
+            androidContext(this@SmartStepApp)
+            modules(appModule)
+        }
+    }
+}
