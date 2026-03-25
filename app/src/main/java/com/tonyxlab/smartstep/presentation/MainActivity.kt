415f1a8366f16dcf4b0ed75ef3326f72a6344e05
Tonnie <vontonnie@gmail.com>
2026-03-25 19:16:27 +0300

    Recalibrate Start Destination

--- app/src/main/java/com/tonyxlab/smartstep/presentation/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/smartstep/presentation/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.tonyxlab.smartstep.data.local.datastore.OnboardingDataStore
@@ -20,8 +23,8 @@ class MainActivity : ComponentActivity() {
 
     val onboardingDataStore: OnboardingDataStore by inject()
 
-    var isAppReady = false
-    var startDestination: NavDestination = OnboardingDestination
+    var isAppReady by mutableStateOf(false)
+    var startDestination by mutableStateOf<NavDestination?>(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +40,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmartStepTheme {
-                SmartStepNavHost(startDestination = startDestination)
+
+                startDestination?.let { destination ->
+                    SmartStepNavHost(startDestination = destination)
+                }
             }
         }
     }
