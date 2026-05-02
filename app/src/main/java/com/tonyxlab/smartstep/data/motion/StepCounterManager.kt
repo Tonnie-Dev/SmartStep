b40f03fd1f4243124837221b1584ed8709353fdf
Tonnie <vontonnie@gmail.com>
2026-05-02 19:27:20 +0300

    Add StepCounterManager Class

---
+++ app/src/main/java/com/tonyxlab/smartstep/data/motion/StepCounterManager.kt
@@ -0,0 +1,119 @@
+@file:RequiresApi(Build.VERSION_CODES.O)
+
+package com.tonyxlab.smartstep.data.motion
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.tonyxlab.smartstep.data.local.datastore.BaselineDataStore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+
+class StepCounterManager(
+    context: Context,
+    private val baselineDataStore: BaselineDataStore,
+    private val scope: CoroutineScope,
+
+) : SensorEventListener {
+
+    private val sensorManager =
+        context.applicationContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    private val stepSensor =
+        sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+    private var baselineSteps: Float? = null
+    private var isBaselineStepsLoaded: Boolean = false
+
+    private val _steps = MutableStateFlow<Int>(0)
+    val steps = _steps.asStateFlow()
+
+    fun isSensorAvailable(): Boolean = stepSensor != null
+
+    fun start() {
+
+        val sensor = stepSensor ?: return
+
+        sensorManager.registerListener(
+                this,
+                sensor,
+                SensorManager.SENSOR_DELAY_UI
+        )
+
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+    }
+
+    /*    fun resetForNewDay() {
+
+            scope.launch {
+
+                baselineSteps?.let { baselineDataStore.setBaselineStepCount(it) }
+                baselineDataStore.clearBaselineStepCount()
+                baselineSteps = null
+                isBaselineStepsLoaded = false
+
+            }
+        }*/
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val sensorCurrentStepsTotal = event.values[0]
+        val today = LocalDate.now()
+        scope.launch {
+
+            val (savedSteps, savedEpochDay) = baselineDataStore.getBaseline()
+            baselineSteps = if (savedEpochDay == today.toEpochDay() && savedSteps > 0) {
+                savedSteps
+            } else {
+                sensorCurrentStepsTotal.also {
+                    baselineDataStore.setBaselineStepCount(
+                            steps = sensorCurrentStepsTotal,
+                            date = today
+                    )
+                }
+            }
+        }
+
+        updateSteps(sensorCurrentStepsTotal)
+
+        /*      if (!isBaselineStepsLoaded) {
+
+                  isBaselineStepsLoaded = true
+
+                  scope.launch {
+
+                      val (savedSteps, savedEpochDay) = baselineDataStore.getBaseline()
+                      val today = LocalDate.now()
+
+                      if (savedEpochDay == today.toEpochDay() && savedSteps > 0) {
+
+                          baselineSteps = savedSteps
+                      } else {
+                          baselineSteps = currentSensorTotal
+                          baselineDataStore.saveBaseline(currentSensorTotal, today)
+                      }
+                  }
+                  updateSteps(totalSteps = currentSensorTotal)
+              } else {
+                  updateSteps(totalSteps = currentSensorTotal)
+              }*/
+    }
+
+    private fun updateSteps(totalSteps: Float) {
+        val todaySteps = (totalSteps - (baselineSteps ?: totalSteps)).toInt()
+                .coerceAtLeast(0)
+
+        _steps.value = todaySteps
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) = Unit
+}
--- app/src/main/java/com/tonyxlab/smartstep/di/AppModule.kt
+++ app/src/main/java/com/tonyxlab/smartstep/di/AppModule.kt
@@ -8,8 +8,10 @@ import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.tonyxlab.smartstep.data.ai.AiClient
 import com.tonyxlab.smartstep.data.ai.AiCoachImpl
+import com.tonyxlab.smartstep.data.local.datastore.BaselineDataStore
 import com.tonyxlab.smartstep.data.local.datastore.OnboardingDataStore
 import com.tonyxlab.smartstep.data.local.datastore.PermPrefsDataStore
+import com.tonyxlab.smartstep.data.motion.StepCounterManager
 import com.tonyxlab.smartstep.data.remote.connectivity.ConnectivityObserverImpl
 import com.tonyxlab.smartstep.data.repository.ActivityStatsImpl
 import com.tonyxlab.smartstep.domain.ai.AiCoach
@@ -24,6 +26,10 @@ import com.tonyxlab.smartstep.presentation.screens.home.handling.ResetExitHandle
 import com.tonyxlab.smartstep.presentation.screens.home.handling.StepsHandler
 import com.tonyxlab.smartstep.presentation.screens.onboarding.OnboardingViewModel
 import com.tonyxlab.smartstep.presentation.screens.report.ReportViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import okhttp3.Dispatcher
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
@@ -36,9 +42,18 @@ val viewModelModule = module {
     viewModelOf(::ReportViewModel)
 }
 
+val repositoryModule = module {
+    single<ActivityStats> { ActivityStatsImpl() }
+}
+
+
+val coroutineScopeModule = module {
+    single<CoroutineScope> { CoroutineScope(SupervisorJob() + Dispatchers .Default) }
+}
 val dataStoreModule = module {
     singleOf(::OnboardingDataStore)
     singleOf(::PermPrefsDataStore)
+    singleOf(::BaselineDataStore)
 }
 
 val connectivityModule = module {
@@ -52,13 +67,19 @@ val connectivityModule = module {
     single<ConnectivityObserver> { ConnectivityObserverImpl(get()) }
 }
 
+val stepCounterManagerModule = module {
+    single<StepCounterManager> {
+        StepCounterManager(
+                context = androidContext(),
+                baselineDataStore = get(),
+                scope = get()
+        )
+    }
+}
+
 val aiCoachModule = module {
     singleOf(::AiClient)
     single<AiCoach> { AiCoachImpl(get()) }
-}
-
-val repositoryModule = module {
-    single<ActivityStats> { ActivityStatsImpl() }
 }
 
 val handlersModule = module {
@@ -75,5 +96,7 @@ val appModule = listOf(
         connectivityModule,
         aiCoachModule,
         repositoryModule,
-        handlersModule
+        handlersModule,
+        coroutineScopeModule,
+        stepCounterManagerModule
 )
