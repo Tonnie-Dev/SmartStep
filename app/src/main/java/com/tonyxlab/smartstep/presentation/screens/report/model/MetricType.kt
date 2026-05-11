92df4812b6c99d91618cb2e78ac33095bf6c7183
Tonnie <vontonnie@gmail.com>
2026-05-11 20:00:53 +0300

    Add formatting and value helpers to MetricType

--- app/src/main/java/com/tonyxlab/smartstep/presentation/screens/report/model/MetricType.kt
+++ app/src/main/java/com/tonyxlab/smartstep/presentation/screens/report/model/MetricType.kt
@@ -1,15 +1,71 @@
 package com.tonyxlab.smartstep.presentation.screens.report.model
 
-enum class MetricType {
-    STEPS, CALORIES, TIME, DISTANCE;
+import com.tonyxlab.smartstep.domain.model.DailyMetric
+import com.tonyxlab.smartstep.utils.formatWithCommas
+import java.util.Locale
 
-    override fun toString(): String {
+enum class MetricType {
+    STEPS,
+    CALORIES,
+    TIME,
+    DISTANCE;
+
+    fun displayName(): String {
         return when (this) {
             STEPS -> "Steps"
-            CALORIES -> "calories"
+            CALORIES -> "Calories"
+            TIME -> "Minutes"
+            DISTANCE -> "Kilometers"
+        }
+    }
+
+    fun unitName(isHeader: Boolean = false): String {
+        return when (this) {
+            STEPS -> "steps"
+            CALORIES -> if (isHeader) "kcal"  else  "calories"
             TIME -> "minutes"
             DISTANCE -> "kilometers"
         }
+    }
+
+    fun getDisplayValue(metric: DailyMetric): String {
+
+        return when (this) {
+            STEPS -> metric.stepCount.toString()
+            CALORIES -> metric.calories.toString()
+            TIME -> (metric.activeSeconds / 60).toString()
+            DISTANCE -> String.format(
+                    Locale.getDefault(),
+                    "%.1f",
+                    metric.distanceKm
+            )
+        }
+
+    }
+    fun formatValue(value: Double): String {
+        return when (this) {
+            STEPS,
+            CALORIES,
+            TIME -> value.toInt().formatWithCommas()
+
+            DISTANCE -> String.format(
+                    Locale.getDefault(),
+                    "%.1f",
+                    value
+            )
+        }
+    }
+
+    fun getNumericValue(metric: DailyMetric): Double {
+        return when (this) {
+            STEPS -> metric.stepCount.toDouble()
+            CALORIES -> metric.calories.toDouble()
+            TIME -> metric.activeSeconds / 60.0
+            DISTANCE -> metric.distanceKm
+        }
+    }
+    override fun toString(): String {
+        return unitName()
     }
 }
 
